833838e69a7f58fc530d35ddb954dc4dd47bc791
Vladimir Bilousko <vladimir.bilousko@namecheap.com>
2019-07-10 15:47:06 +0300

    report

--- Hosts/Falcon.Hosts.Report/Consumers/ReportConsumer.cs
+++ Hosts/Falcon.Hosts.Report/Consumers/ReportConsumer.cs
@@ -1,17 +1,33 @@
-using System;
 using System.Threading.Tasks;
+using EasyNetQ;
 using EasyNetQ.AutoSubscribe;
 using Falcon.Profiles.Report;
+using Falcon.Profiles.Telegram;
 
 namespace Falcon.Hosts.Report.Consumers
 {
     public class ReportConsumer : IConsumeAsync<ReportProfile>
     {
-        public Task ConsumeAsync(ReportProfile message)
-        {
-            message.Reports.ForEach(model => Console.WriteLine(model.Output));
+        private readonly IBus _bus;
 
-            return Task.CompletedTask;
+        public ReportConsumer(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task ConsumeAsync(ReportProfile profile)
+        {
+            await PublishTelegramProfile(profile);
+        }
+
+        private async Task PublishTelegramProfile(ReportProfile profile)
+        {
+            await _bus.PublishAsync(new TelegramProfile
+            {
+                Context = profile.Context,
+                Target = profile.Target,
+                Reports = profile.Reports,
+            });
         }
     }
 }
---
+++ Profiles/Falcon.Profiles.Telegram/TelegramProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Falcon.Profiles.Telegram
+{
+    public class TelegramProfile : ITargetProfile
+    {
+        public SessionContext Context { get; set; }
+        public string Target { get; set; }
+
+        public List<ReportModel> Reports { get; set; }
+    }
+}
